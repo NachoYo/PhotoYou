d473484eae3002fcce00119c579f4a560d41b5b0
Luis Ignacio Viñas Petriz <luis.ignacio.vinas.petriz@everis.com>
2021-04-10 21:31:49 -0500

    Commit creación proyecto

---
+++ PhotoYou/PhotoYou/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoYou
+//
+//  Created by Luis Ignacio Viñas Petriz on 10/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
