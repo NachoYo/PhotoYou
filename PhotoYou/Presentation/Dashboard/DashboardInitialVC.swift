dc9d09e0d87725aedffe14f94d971acd87665797
Luis Ignacio Viñas Petriz <luis.ignacio.vinas.petriz@everis.com>
2021-04-10 23:44:59 -0500

    Commit creacion dashboard

---
+++ PhotoYou/Presentation/Dashboard/DashboardInitialVC.swift
@@ -0,0 +1,19 @@
+//
+//  DashboardInitialVC.swift
+//  PhotoYou
+//
+//  Created by Luis Ignacio Viñas Petriz on 10/04/21.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class DashboardInitialVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = true
+    }
+
+}
